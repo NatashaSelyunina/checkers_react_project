ca8b32006e81f838cc31e8b51a24703292ab669b
Natalya.Selyunina <133572779+NatashaSelyunina@users.noreply.github.com>
2023-12-11 17:44:24 +0300

    add BoardComponents, CellComponents

---
+++ src/components/BoardComponent.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+//import cn from 'classnames'
+//import styles from './BoardComponent.module.css'
+
+const BoardComponent: FC = () => {
+
+  return (
+    <div className="board">
+    
+    </div>
+  );
+};
+
+export default BoardComponent;
---
+++ src/components/CellComponents.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+//import cn from 'classnames'
+//import styles from './CellComponents.module.css'
+
+const CellComponents: FC = () => {
+
+  return (
+    <div className="cell">
+      
+    </div>
+  );
+};
+
+export default CellComponents;
