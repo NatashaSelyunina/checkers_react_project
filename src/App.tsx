691cd2320e29e13442db6d680857f0cd7311adb8
Natalya.Selyunina <133572779+NatashaSelyunina@users.noreply.github.com>
2023-12-11 18:13:42 +0300

    add function restart() for create Board

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import BoardComponent from "./components/BoardComponent";
+import { Board } from "./logic/Board";
 
 const App = () => {
+  const [board, setBoard] = useState(new Board());
+
+  useEffect(() => {
+    restart();
+  }, [])
+
+  function restart() {
+    const newBoard = new Board();
+    newBoard.createCells();
+    setBoard(newBoard);
+  }
+
   return (
     <div className="app">
      <BoardComponent/>
