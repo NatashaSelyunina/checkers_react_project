83fc8f305b4bc9a929b51b98b886f979bcead58f
Natalya.Selyunina <133572779+NatashaSelyunina@users.noreply.github.com>
2023-12-11 18:09:08 +0300

    create fiels and constructor for class Cell, fix createCells() in Board

--- src/logic/Cell.ts
+++ src/logic/Cell.ts
@@ -1,3 +1,23 @@
+import { Board } from "./Board";
+import { Colors } from "./Colors";
+import { Checker } from "./checkers/Checker";
+
 export class Cell {
-  
+  readonly x: number;
+  readonly y: number;
+  readonly color: Colors;
+  checker: Checker | null;
+  board: Board;
+  free: boolean;
+  id: number;
+
+  constructor(board: Board, x: number, y: number, color: Colors, checker: Checker | null) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.checker = checker;
+    this.board = board;
+    this.free = false;
+    this.id = Math.random();
+  }
 }
