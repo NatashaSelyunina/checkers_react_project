1da09622a840ccf94cf81d141d2c50c3d94057d3
Natalya.Selyunina <133572779+NatashaSelyunina@users.noreply.github.com>
2023-12-11 17:58:35 +0300

    add createCells() for Board, add enum Colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import BoardComponent from "./components/BoardComponent";
 
 const App = () => {
   return (
     <div className="app">
-     
+     <BoardComponent/>
     </div>
   );
 };
