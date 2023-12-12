d48a15abdb9aa3979ce526f2c401346d889d9e3a
Natalya.Selyunina <133572779+NatashaSelyunina@users.noreply.github.com>
2023-12-12 08:04:10 +0300

    fix add figures with array

--- src/logic/Board.ts
+++ src/logic/Board.ts
@@ -25,43 +25,20 @@ export class Board {
   }
 
   private addCheckers() {
-    /*for (let i = 0; i < 8; i++) {
-      const row: Cell[] = [];
+    for (let i = 0; i < 8; i++) {
       for (let j = 0; j < 3; j++) {
         if ((i + j) % 2 !== 0) {
-          row.push(new Checker());
-        } else {
-          row.push(new Cell(this, j, i, Colors.WHITE, null));
-        }
+          new Checker(Colors.BLACK, this.getCell(i, j));
+        } 
       }
-
-      this.cells.push(row);
-    }*/
-    new Checker(Colors.BLACK, this.getCell(1, 0))
-    new Checker(Colors.BLACK, this.getCell(3, 0))
-    new Checker(Colors.BLACK, this.getCell(5, 0))
-    new Checker(Colors.BLACK, this.getCell(7, 0))
-    new Checker(Colors.BLACK, this.getCell(0, 1))
-    new Checker(Colors.BLACK, this.getCell(2, 1))
-    new Checker(Colors.BLACK, this.getCell(4, 1))
-    new Checker(Colors.BLACK, this.getCell(6, 1))
-    new Checker(Colors.BLACK, this.getCell(1, 2))
-    new Checker(Colors.BLACK, this.getCell(3, 2))
-    new Checker(Colors.BLACK, this.getCell(5, 2))
-    new Checker(Colors.BLACK, this.getCell(7, 2))
-
-    new Checker(Colors.WHITE, this.getCell(0, 5))
-    new Checker(Colors.WHITE, this.getCell(2, 5))
-    new Checker(Colors.WHITE, this.getCell(4, 5))
-    new Checker(Colors.WHITE, this.getCell(6, 5))
-    new Checker(Colors.WHITE, this.getCell(1, 6))
-    new Checker(Colors.WHITE, this.getCell(3, 6))
-    new Checker(Colors.WHITE, this.getCell(5, 6))
-    new Checker(Colors.WHITE, this.getCell(7, 6))
-    new Checker(Colors.WHITE, this.getCell(0, 7))
-    new Checker(Colors.WHITE, this.getCell(2, 7))
-    new Checker(Colors.WHITE, this.getCell(4, 7))
-    new Checker(Colors.WHITE, this.getCell(6, 7))
+    }
+    for (let i = 0; i < 8; i++) {
+      for (let j = 5; j < 8; j++) {
+        if ((i + j) % 2 !== 0) {
+          new Checker(Colors.WHITE, this.getCell(i, j));
+        } 
+      }
+    }
   }
 
   public addFigures() {
